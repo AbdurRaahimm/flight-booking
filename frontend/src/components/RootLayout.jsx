18902e3d50043374a75be99f9fa9169aa1411f55
“AbdurRaahimm” <rahim703936@gmail.com>
2025-01-09 22:43:58 +0600

    scroll to top on route change;

--- frontend/src/components/RootLayout.jsx
+++ frontend/src/components/RootLayout.jsx
@@ -1,16 +1,21 @@
-import React from 'react'
-import Navbar from './Navbar'
-import Footer from './Footer'
-import { Outlet } from 'react-router-dom'
+import { useEffect } from "react";
+import Navbar from "./Navbar";
+import Footer from "./Footer";
+import { Outlet, useLocation } from "react-router-dom";
 
 export default function RootLayout() {
-    return (
-        <>
-            <Navbar />
-            <main className="">
-                <Outlet />
-            </main>
-            <Footer />
-        </>
-    )
+  const location = useLocation();
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [location]);
+  
+  return (
+    <>
+      <Navbar />
+      <main className="">
+        <Outlet />
+      </main>
+      <Footer />
+    </>
+  );
 }
